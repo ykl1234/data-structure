9a4b96fb431bc40f3612d0cdded922f1c285ec0a
Keyang Xiao <116143413+ykl1234@users.noreply.github.com>
2024-03-24 18:41:52 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,86 +1,81 @@
-# main.py
 import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox
-from first_windows import Ui_Dialog as Ui_FirstWindow
-from sign_in_windows import Ui_Dialog as Ui_SignInWindow
-from sign_up_windows import Ui_Dialog as Ui_SignUpWindow
-from main_windows import Ui_Dialog as Ui_MainWindow
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QApplication, QDialog, QMessageBox, QMainWindow
+from first_windows import Ui_first_windows
+from sign_in_windows import Ui_sign_in_windows
+from sign_up_windows import Ui_sign_up_windows_2
+from main_windows import Ui_main_windows
 from user_info import UserInfo
 
-
-class AppWindow(QDialog):
-    def __init__(self, ui_class, userInfo):
+class FirstWindow(QDialog):
+    def __init__(self):
         super().__init__()
-        self.ui = ui_class()
+        self.ui = Ui_first_windows()
         self.ui.setupUi(self)
-        self.userInfo = userInfo
+        self.ui.pushButton.clicked.connect(self.showSignInWindow)
+        self.ui.pushButton_2.clicked.connect(self.showSignUpWindow)
 
-
-class FirstWindow(AppWindow):
-    def __init__(self, userInfo):
-        super().__init__(Ui_FirstWindow, userInfo)
-        self.ui.pushButton.clicked.connect(self.openSignInWindow)
-        self.ui.pushButton_2.clicked.connect(self.openSignUpWindow)
-
-    def openSignInWindow(self):
-        self.window = SignInWindow(self.userInfo)
-        self.window.show()
-
-    def openSignUpWindow(self):
-        self.window = SignUpWindow(self.userInfo)
-        self.window.show()
-
-
-class SignInWindow(AppWindow):
-    def __init__(self, userInfo):
-        super().__init__(Ui_SignInWindow, userInfo)
-        self.ui.pushButton.clicked.connect(self.signIn)
-        self.ui.pushButton_2.clicked.connect(self.close)
-
-    def signIn(self):
-        name = self.ui.textEdit.toPlainText()
-        password = self.ui.textEdit_2.toPlainText()
-        success, message = self.userInfo.SignIn(name, password)
-        if success:
-            self.window = MainWindow()
-            self.window.show()
-            self.close()
-        else:
-            QMessageBox.warning(self, 'Error', message)
-
-
-class SignUpWindow(AppWindow):
-    def __init__(self, userInfo):
-        super().__init__(Ui_SignUpWindow, userInfo)
-        self.ui.pushButton.clicked.connect(self.signUp)
-        self.ui.pushButton_2.clicked.connect(self.close)
-
-    def signUp(self):
-        name = self.ui.textEdit.toPlainText()
-        password = self.ui.textEdit_2.toPlainText()
-        success, message = self.userInfo.SignUp(name, password)
-        if success:
-            QMessageBox.information(self, 'Success', message)
-            self.close()  # 关闭注册窗口
-            self.openSignInWindow()  # 打开登录窗口
-        else:
-            QMessageBox.warning(self, 'Error', message)
-
-    def openSignInWindow(self):
-        self.signInWindow = SignInWindow(self.userInfo)
+    def showSignInWindow(self):
+        self.signInWindow = SignInWindow()
+        self.signInWindow.loginSuccess.connect(self.close)
         self.signInWindow.show()
 
+    def showSignUpWindow(self):
+        self.signUpWindow = SignUpWindow()
+        self.signUpWindow.signupSuccess.connect(self.close)
+        self.signUpWindow.show()
 
-class MainWindow(AppWindow):
+class SignInWindow(QDialog):
+    loginSuccess = pyqtSignal()
+
     def __init__(self):
-        super().__init__(Ui_MainWindow, None)
+        super().__init__()
+        self.ui = Ui_sign_in_windows()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.login)
+        self.ui.pushButton_2.clicked.connect(self.close)
 
+    def login(self):
+        username = self.ui.lineEdit.text()
+        password = self.ui.lineEdit_2.text()
+        success, msg = user_info.SignIn(username, password)
+        QMessageBox.information(self, "登录结果", msg)
+        if success:
+            self.loginSuccess.emit()
+            self.mainWindow = MainWindow()
+            self.mainWindow.show()
+            self.close()
 
-# MainWindow doesn't require interaction with userInfo directly, so it passes None
+class SignUpWindow(QDialog):
+    signupSuccess = pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_sign_up_windows_2()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.signup)
+        self.ui.pushButton_2.clicked.connect(self.close)
+
+    def signup(self):
+        username = self.ui.lineEdit.text()
+        password = self.ui.sign_up_windows.text()
+        success, msg = user_info.SignUp(username, password)
+        QMessageBox.information(self, "注册结果", msg)
+        if success:
+            self.signupSuccess.emit()
+            self.mainWindow = MainWindow()
+            self.mainWindow.show()
+            self.close()
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_main_windows()
+        self.ui.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    userInfo = UserInfo()
-    firstWindow = FirstWindow(userInfo)
+    user_info = UserInfo()  # Create an instance of UserInfo
+    firstWindow = FirstWindow()
     firstWindow.show()
     sys.exit(app.exec_())
